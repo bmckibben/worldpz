21c3268ddf0ccf1268544d359ea088b4d9de6f2b
bbmckibb <bbmckibb@iu.edu>
2017-02-07 15:27:46 -0500

    creating database

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
-
+  root 'home#index'
+  
+  get 'home/index' => 'home#index', :as => 'home'
+  
   resources :recipe_ingredients
   resources :recipe_ingredients_units
   resources :recipe_steps_ingredients
@@ -9,8 +12,6 @@ Rails.application.routes.draw do
   resources :recipe_images
   resources :recipes
   resources :recipe_categories
-  root 'home#index'
-  
-  get 'home/index' => 'home#index', :as => 'home'
+
      
 end
