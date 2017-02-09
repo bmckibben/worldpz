f027a726caac6687ab23f6f3e62fdf60183f7a59
Ben McKibben <mckibben@att.net>
2017-02-08 20:48:22 -0500

    admin page

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,10 @@
 Rails.application.routes.draw do
 
-  resources :recipe_ingredients_units
-  resources :recipe_ingredients
-  resources :recipe_steps_ingredients
-  resources :recipe_steps
-  resources :recipe_images
-  resources :recipes
-  resources :recipe_categories
-  resources :recipe_steps
-  resources :recipe_images
-  resources :recipes
-  resources :recipe_categories
-  resources :recipe_ingredients_units
-  resources :recipe_ingredients
-  resources :recipe_steps_ingredients
-  resources :recipe_steps
-  resources :recipe_images
-  resources :recipes
-  resources :recipe_categories
   devise_for :users
   root 'home#index'
   
   get 'home/index' => 'home#index', :as => 'home'
+  get 'admin/index' => 'admin#index', :as => 'admin'
   
   resources :recipe_ingredients
   resources :recipe_ingredients_units
