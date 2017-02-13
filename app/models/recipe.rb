eb7d8c536f277eabc8b7edb27de207100b3b223e
bbmckibb <bbmckibb@iu.edu>
2017-02-13 16:09:33 -0500

    style recipe children

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,7 @@
 class Recipe < ActiveRecord::Base
 	belongs_to :recipe_category
 	has_many :recipe_images
-	has_many :recipe_steps
+	has_many :recipe_steps, :dependent => :destroy
+	accepts_nested_attributes_for :recipe_steps, :reject_if => lambda { |a| a[:name].blank? }, :allow_destroy => true
+
 end
