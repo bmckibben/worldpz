c8ba2bfcc88bf58e3cbbaeed932350c2bc4bcd2a
bbmckibb <bbmckibb@iu.edu>
2017-02-20 14:33:28 -0500

    fix link_to_function bug

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -1,7 +1,7 @@
 module RecipesHelper
 
   def link_to_remove_fields(name, f)
-    f.hidden_field(:_destroy) + link_to_function(name, "remove_fields(this)")
+    f.hidden_field(:_destroy) + link_to(name, "remove_fields(this)")
   end
   
   def link_to_add_fields(name, f, association)
