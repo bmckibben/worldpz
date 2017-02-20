674af18f85aea8e6aabcc9cc92dd272ea0ca7c50
bbmckibb <bbmckibb@iu.edu>
2017-02-20 15:08:07 -0500

    getting children to behave

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -1,7 +1,9 @@
 module RecipesHelper
 
   def link_to_remove_fields(name, f)
-    f.hidden_field(:_destroy) + link_to(name, "remove_fields(this)")
+    f.hidden_field(:_destroy) + link_to(name, "remove_fields(this)") do
+      <i aria-hidden="false" class="glyphicon glyphicon-pencil"></i>
+    end  
   end
   
   def link_to_add_fields(name, f, association)
