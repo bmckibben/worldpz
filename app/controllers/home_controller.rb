8fa4d195e99470ffc7fa43fd5abb5a44a0f2dcf0
bbmckibb <bbmckibb@iu.edu>
2017-02-24 15:38:57 -0500

    plug blog into home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
   
   def index
     @recipes = Recipe.where("category_id = 1 and sort_order > 0").order(:sort_order)
+    @blog = Blog.order(created_at: :desc).first
   	render layout: false
   end
   
@@ -13,4 +14,5 @@ class HomeController < ApplicationController
   	HomeMailer.send_contact_email(params[:email_name],params[:email_address],params[:email_message]).deliver_now
   	render :nothing => true, :status => 200
   end	
+
 end
