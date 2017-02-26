283bdbd45dffbf82a0c6407b63f56d2858d84439
Ben McKibben <mckibben@att.net>
2017-02-26 14:15:27 -0500

    angularizing blog reply

--- public/assets/application-0ca96b661687512b0fb5115364246ca5e20c3940c51138767610310d476da291.js
+++ public/assets/application-0ca96b661687512b0fb5115364246ca5e20c3940c51138767610310d476da291.js
@@ -45488,6 +45488,25 @@ $(document).ready(function() {
 		$("#reply_"+id).toggle();
 	});	
 });
+
+angular.module('appBlogReply', [])
+.controller("ctlrBlogReply", function($scope, $http, $interval){
+ 
+
+      $scope.postReply = function(id,body){
+                 
+            $http.post("/blog_comments/create",  
+                  {"body":body,"commentable_id":parent_id }
+                  )
+                  .success(function(data){
+                              alert("Your reply has been sent! It will be published if approved.");
+                  })
+                  .error(function(data){
+                              alert("Your reply was NOT sent!");
+                  });
+      };
+ 
+});
 angular.module('appContact', [])
 .controller("ctlrContact", function($scope, $http, $interval){
  
